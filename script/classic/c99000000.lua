4bcd4016d196d1b2e60df7d1aa3309e3f72bf10f
MRX <mcrayan93@gmail.com>
2026-07-07 11:49:40 +0200

    Update c99000000.lua

--- script/classic/c99000000.lua
+++ script/classic/c99000000.lua
@@ -1,24 +1,70 @@
---Switcheroo
+-- Nom de ton Skill Secret
+-- Scripted by TonNom
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+    -- Procédure officielle pour les cartes Skill
+    aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
+
+-- Filtre pour vérifier si tu contrôles au moins un monstre "Numéro" (Archétype 0x48)
+function s.num_control_filter(c)
+    return c:IsFaceup() and c:IsSetCard(0x48)
+end
+
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--once per duel check
-	if Duel.GetFlagEffect(ep,id)>0 then return end
-	--condition
-	return aux.CanActivateSkill(tp) and Duel.IsPlayerCanDraw(tp,1)
+    if not aux.CanActivateSkill(tp) then return false end
+    if Duel.GetTurnPlayer()~=tp then return false end
+    
+    local c=e:GetHandler()
+    
+    -- ÉTAPE 1 : Si le Skill est face verso, on peut le retourner
+    if c:IsFacedown() then return true end
+    
+    -- ÉTAPE 2 : Si le Skill est déjà face recto, on peut faire l'effet (1 fois par duel)
+    -- Optionnel : j'ai ajouté une vérification pour s'assurer que tu as bien un "Numéro" sur ton terrain pour pouvoir l'activer
+    return Duel.GetFlagEffect(tp,id)==0 
+        and Duel.IsExistingMatchingCard(s.num_control_filter,tp,LOCATION_MZONE,0,1,nil)
 end
+
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_CARD,tp,id)
-	--used skill flag register
-	Duel.RegisterFlagEffect(ep,id,0,0,0)
-	local c=e:GetHandler()
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
-	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
-		Duel.ShuffleDeck(tp)
-		Duel.BreakEffect()
-		Duel.Draw(tp,1,REASON_EFFECT)
-	end
+    local c=e:GetHandler()
+    
+    -- Cas 1 : Le Skill est face verso -> On le met juste face recto
+    if c:IsFacedown() then
+        Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+        Duel.Hint(HIND_CARD,tp,id)
+        return
+    end
+    
+    -- Cas 2 : Le Skill est déjà face recto -> Activation de l'effet de protection
+    if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
+    
+    -- On verrouille le Skill pour le reste du duel
+    Duel.RegisterFlagEffect(tp,id,0,0,0)
+    
+    -- Animation visuelle de l'activation de l'effet
+    Duel.Hint(HINT_CARD,tp,id)
+    
+    -- CRÉATION DE LA PROTECTION (Effet persistant sur le Terrain)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_INDESTRUCTIVABLE_BATTLE)
+    e1:SetTargetRange(LOCATION_MZONE,0) -- S'applique sur tes monstres
+    e1:SetTarget(s.protection_tg)       -- Uniquement les monstres "Numéro"
+    e1:SetValue(s.battle_value)         -- Sauf contre un autre "Numéro"
+    
+    -- Durée de l'effet : Jusqu'à la fin du prochain tour de l'adversaire
+    -- Si c'est actuellement ton tour, le prochain tour de l'adversaire est le tour actuel + 1 (donc 1 cycle complet de son tour)
+    e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN,1)
+    Duel.RegisterEffect(e1,tp)
+end
+
+-- Cible de la protection : Tous tes monstres qui appartiennent à l'archétype "Numéro"
+function s.protection_tg(e,c)
+    return c:IsSetCard(0x48)
+end
+
+-- Règle de combat : Immunisé contre la destruction, SAUF si le monstre attaquant (c) est aussi un "Numéro"
+function s.battle_value(e,c)
+    return not c:IsSetCard(0x48)
 end
