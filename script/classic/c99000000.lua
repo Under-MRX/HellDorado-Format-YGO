938cd173772d59ddd4bc2f081873c24ad5840b02
MRX <mcrayan93@gmail.com>
2026-07-07 15:27:38 +0200

    Update c99000000.lua

--- script/classic/c99000000.lua
+++ script/classic/c99000000.lua
@@ -8,14 +8,12 @@ function s.initial_effect(c)
 end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.(ep,id)>0 then return end
     return aux.CanActivateSkill(tp)
 end
 
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
     Duel.Hint(HINT_CARD,tp,id)
-    Duel.RegisterFlagEffect(ep,id,0,0,0)
     
     --Lancer de dé
     local dice = Duel.TossDice(tp, 1)
