4befb9aad85ace5379642edb8b1352868e2fecc5
MRX <mcrayan93@gmail.com>
2026-07-07 12:32:09 +0200

    Update c99000000.lua

--- script/classic/c99000000.lua
+++ script/classic/c99000000.lua
@@ -12,6 +12,7 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	return aux.CanActivateSkill(tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
 
