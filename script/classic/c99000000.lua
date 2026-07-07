ba65a252702a82eb3f94ce87fad823c595d48ba6
MRX <mcrayan93@gmail.com>
2026-07-07 12:30:33 +0200

    Update c99000000.lua

--- script/classic/c99000000.lua
+++ script/classic/c99000000.lua
@@ -1,20 +1,22 @@
 -- Skill Universal
 
 local s,id=GetID()
+local SET_NUMBER = 0x48
+
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
+
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(ep,id)>0 then return end
+	if Duel.GetFlagEffect(tp,id)>0 then return end
 	return aux.CanActivateSkill(tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
 
 	local c=e:GetHandler()
-local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_INDESTRUCTIVABLE_BATTLE)
     e1:SetTargetRange(LOCATION_MZONE,0)
