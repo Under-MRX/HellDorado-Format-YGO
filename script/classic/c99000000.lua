abf77bec73e44d3f3d45d68dfb49b3278248cd68
MRX <mcrayan93@gmail.com>
2026-07-07 12:27:09 +0200

    Update c99000000.lua

--- script/classic/c99000000.lua
+++ script/classic/c99000000.lua
@@ -1,38 +1,34 @@
+-- Skill Universal
+
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--once per duel check
 	if Duel.GetFlagEffect(ep,id)>0 then return end
-	--condition
-	return aux.CanActivateSkill(tp) and Duel.IsPlayerCanDraw(tp,1)
+	return aux.CanActivateSkill(tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--used skill flag register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
-    
-    -- CRÉATION DE LA PROTECTION (Effet persistant sur le Terrain)
-    local e1=Effect.CreateEffect(c)
+
+	local c=e:GetHandler()
+local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_INDESTRUCTIVABLE_BATTLE)
-    e1:SetTargetRange(LOCATION_MZONE,0) 
-	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
-	c:RegisterEffect(e1)
-    -- CORRECTION ERREUR PARAMETER 2 : On évite RESET_OPPO_TURN. 
-    -- 2 End Phases = Ton tour actuel + Le tour complet de l'adversaire.
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    
+    e1:SetTarget(s.num_target) 
+    
+    e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+    
     e1:SetReset(RESET_PHASE+PHASE_END,2)
+    
     Duel.RegisterEffect(e1,tp)
+
+end
+function s.num_target(e,c)
+    return c:IsFaceup() and c:IsSetCard(SET_NUMBER)
 end
 
--- Cible de la protection : Tous tes monstres qui appartiennent à l'archétype "Numéro"
-function s.protection_tg(e,c)
-    return c:IsSetCard(0x48)
-end
-
--- Règle de combat : Immunisé (true), SAUF si l'attaquant (c) est un "Numéro" (false)
-function s.battle_value(e,c)
-    return not c:IsSetCard(0x48)
-end
