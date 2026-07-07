b3d6d3d73c66c0a0a52f7770ed007b05738e35f1
MRX <mcrayan93@gmail.com>
2026-07-07 15:09:03 +0200

    Update c99000000.lua

--- script/classic/c99000000.lua
+++ script/classic/c99000000.lua
@@ -1,55 +1,34 @@
 -- Skill Universal
 local s,id=GetID()
-local SET_NUMBER = 0x48
 
 function s.initial_effect(c)
     aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetFlagEffect(tp,id)>0 then return false end
+    if Duel.GetFlagEffect(ep,id)>0 then return end
     return aux.CanActivateSkill(tp)
 end
 
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
     Duel.Hint(HINT_CARD,tp,id)
-    Duel.RegisterFlagEffect(tp,id,0,0,0)
+    Duel.RegisterFlagEffect(ep,id,0,0,0)
 
     local c=e:GetHandler()
-    local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_INDESTRUCTIVABLE_BATTLE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
     e1:SetTargetRange(LOCATION_MZONE,0)
-    
-    -- Le filtre magique est ici : on calcule tout dans le Target
-    e1:SetTarget(s.protection_tg) 
-    
-    -- On force la valeur à 1 (un Int pur), ce qui signifie "Invincible" de base
-    e1:SetValue(1)
-    
+   
+    e1:SetTarget(s.num_target)
+    e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+
     e1:SetReset(RESET_PHASE+PHASE_END,2)
     Duel.RegisterEffect(e1,tp)
 end
 
--- La logique est déplacée ici pour éviter le bug de type :
-function s.protection_tg(e,c)
-    -- ÉTAPE 1 : Le monstre protégé doit être un "Numéro" à toi
-    if not (c:IsFaceup() and c:IsSetCard(SET_NUMBER)) then return false end
-    
-    -- ÉTAPE 2 : On récupère le monstre qui l'attaque ou qu'il attaque
-    local a=Duel.GetAttacker()
-    local d=Duel.GetAttackTarget()
-    
-    -- Si un combat est en cours, on cherche le monstre adverse
-    local opp = nil
-    if a == c then opp = d end
-    if d == c then opp = a end
-    
-    -- Si l'adversaire est un monstre "Numéro", la protection NE S'APPLIQUE PAS (on renvoie false)
-    if opp and opp:IsSetCard(SET_NUMBER) then
-        return false
-    end
-    
-    -- Dans tous les autres cas (pas un numéro, ou pas de combat), la protection est active
-    return true
+function s.num_target(e,c)
+    return c:IsFaceup() and c:IsSetCard(SET_NUMBER)
 end
+
